18a9fbc764751a86292c2287226b39e79c8e194d
Artem Nefedov <anefedov@aligntech.com>
2020-06-05 13:17:07 +0300

    Wildcard url

--- AWSClickSSOButton.js
+++ AWSClickSSOButton.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  Just click the damn button...
 // @author       Artem Nefedov
-// @match        https://aligntech.awsapps.com/start/user-consent/authorize.html?clientId=*
+// @match        https://*.awsapps.com/start/user-consent/authorize.html?clientId=*
 // @grant        none
 // ==/UserScript==
 
--- AWSCloseSSOWindow.js
+++ AWSCloseSSOWindow.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  Just close the damn window...
 // @author       Artem Nefedov
-// @match        https://aligntech.awsapps.com/start/user-consent/login-success.html
+// @match        https://*.awsapps.com/start/user-consent/login-success.html
 // @grant        window.close
 // ==/UserScript==
 
